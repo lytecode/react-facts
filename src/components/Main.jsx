9e4fe57bc658b7d2fb8206166868c5799ca5e452
Mbonu Basil <mbonubasil@gmail.com>
2024-01-17 16:17:40 -0500

    Add the main content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-    return <h1>Main Component</h1>
+    return (
+        <main className='main'>
+            <h1 className="title">Fun facts about React</h1>
+            <ul className="points">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
 }
